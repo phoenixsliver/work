292e5818ee3a7a2bd0bfda6a7991ba664fd70979
phoenixhell <phoenixrever@gmail.com>
2022-06-25 19:17:04 +0900

    TestCICD

--- src/test/java/io/renren/JwtTest.java
+++ src/test/java/io/renren/JwtTest.java
@@ -1,14 +1,15 @@
 package io.renren;
 
 import io.renren.modules.app.utils.JwtUtils;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-
-@RunWith(SpringRunner.class)
+/**
+ * 新spring boot 不用junit4 了 用junit5（jupiter）了 不需要这个注解
+ */
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class JwtTest {
     @Autowired
@@ -17,7 +18,7 @@ public class JwtTest {
     @Test
     public void test() {
         String token = jwtUtils.generateToken(1);
-
+        int a=10/0;
         System.out.println(token);
     }
 
